ae35f7982803637f1e5d74e77641a518834bbf1d
Prasanna Annamalai <prannamalai.x.com@gmail.com>
2013-04-01 15:39:20 +0530

    Modified Rest Wrapper, Payment Test Case

--- test/payment.js
+++ test/payment.js
@@ -1,7 +1,7 @@
 /**
  * New node file
  */
- 
+
 var chai = require('chai'),
 	expect = chai.expect,
 	should = chai.should();
@@ -11,12 +11,7 @@ var paypal_sdk = require('../');
 
 var http_default_opts = {
 	'host': 'api.sandbox.paypal.com',
-	'port': '',
-	'headers': {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-		'Authorization': ''
-	}
+	'port': ''
 };
 
 paypal_sdk.configure(http_default_opts);
@@ -24,39 +19,6 @@ paypal_sdk.configure(http_default_opts);
 var client_id = 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM';
 var client_secret = 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM';
 
-
-describe('SDK', function() {
-	describe('#createpaymentusingcard', function() {
-		it('payment should be created', function(done) {
-			paypal_sdk.generateToken(client_id, client_secret, function(generatedToken) {
-				console.log("The Generated Token is " + generatedToken);
-
-				http_default_opts.headers['Authorization'] = generatedToken;
-
-				paypal_sdk.payment.create(create_payment_json_card,http_default_opts, function(res, err) {
-					if (err) {
-						throw err;
-					}
-
-					if (res) {
-						expect(res.id).to.contain('PAY-');
-						done();
-						paypal_sdk.payment.get(res.id, http_default_opts, function(get_res, get_err) {
-				if (get_err) {
-					throw get_res;
-				}
-
-				if (get_res) {
-					expect(get_res.state).to.contain('approved');
-				}
-
-			});
-					}
-				});
-			})
-		})
-	})
-})
 var create_payment_json_card = {
 	"intent": "sale",
 	"payer": {
@@ -93,9 +55,72 @@ var create_payment_json_card = {
 		"description": "This is the payment transaction description."
 	}]
 };
-	
-	
-	describe('SDK', function() {
+
+
+
+describe('SDK', function() {
+	describe('#createpaymentusingcard', function() {
+		it('payment should be created', function(done) {
+			paypal_sdk.generateToken(client_id, client_secret, function(generatedToken) {
+				console.log("The Generated Token is " + generatedToken);
+
+				if (!http_default_opts.headers) {
+					http_default_opts['headers'] = {};
+				}
+
+				http_default_opts.headers['Authorization'] = generatedToken;
+
+				paypal_sdk.payment.create(create_payment_json_card, http_default_opts, function(res, err) {
+					if (err) {
+						throw err;
+					}
+
+					if (res) {
+						expect(res.id).to.contain('PAY-');
+						paypal_sdk.payment.get(res.id, http_default_opts, function(get_res, get_err) {
+							if (get_err) {
+								throw get_res;
+							}
+
+							if (get_res) {
+								expect(get_res.state).to.contain('approved');
+								done();
+							}
+
+						});
+					}
+				});
+			})
+		})
+	})
+})
+
+
+var create_payment_json_savedcard = {
+	"intent": "sale",
+	"payer": {
+		"payment_method": "credit_card",
+		"funding_instruments": [{
+			"credit_card_token": {
+				"credit_card_id": "CARD-3SP328368F401020AKFKCYUQ"
+			}
+		}]
+	},
+	"transactions": [{
+		"amount": {
+			"total": "7",
+			"currency": "USD",
+			"details": {
+				"subtotal": "5",
+				"tax": "1",
+				"shipping": "1"
+			}
+		},
+		"description": "This is the payment transaction description."
+	}]
+};
+
+describe('SDK', function() {
 	describe('#createpaymentusingsavedcard', function() {
 		it('payment should be created', function(done) {
 			paypal_sdk.generateToken(client_id, client_secret, function(generatedToken) {
@@ -103,7 +128,7 @@ var create_payment_json_card = {
 
 				http_default_opts.headers['Authorization'] = generatedToken;
 
-				paypal_sdk.payment.create(create_payment_json_savedcard,http_default_opts, function(res, err) {
+				paypal_sdk.payment.create(create_payment_json_savedcard, http_default_opts, function(res, err) {
 					if (err) {
 						throw err;
 					}
@@ -117,33 +142,31 @@ var create_payment_json_card = {
 		})
 	})
 })
-var create_payment_json_savedcard = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "credit_card",
-        "funding_instruments": [
-            {
-                "credit_card_token": {
-                    "credit_card_id": "CARD-3SP328368F401020AKFKCYUQ"
-                }
-            }
-        ]
-    },
-    "transactions": [
-        {
-            "amount": {
-                "total": "7",
-                "currency": "USD",
-                "details": {
-                    "subtotal": "5",
-                    "tax": "1",
-                    "shipping": "1"
-                }
-            },
-            "description": "This is the payment transaction description."
-        }
-    ]
+
+
+var create_payment_json_paypal = {
+	"intent": "sale",
+	"payer": {
+		"payment_method": "paypal"
+	},
+	"transactions": [{
+		"amount": {
+			"total": "7",
+			"currency": "USD",
+			"details": {
+				"subtotal": "5",
+				"tax": "1",
+				"shipping": "1"
+			}
+		},
+		"description": "This is the payment transaction description."
+	}],
+	"redirect_urls": {
+		"return_url": "http://localhost/paymentwithpaypal?guid=b64644f236ed484fb047ec3439fb7a3f",
+		"cancel_url": "http://localhost/paymentwithpaypal?guid=b64644f236ed484fb047ec3439fb7a3f"
+	}
 };
+
 describe('SDK', function() {
 	describe('#createpaymentusingpaypal', function() {
 		it('payment should be created', function(done) {
@@ -152,7 +175,7 @@ describe('SDK', function() {
 
 				http_default_opts.headers['Authorization'] = generatedToken;
 
-				paypal_sdk.payment.create(create_payment_json_paypal,http_default_opts, function(res, err) {
+				paypal_sdk.payment.create(create_payment_json_paypal, http_default_opts, function(res, err) {
 					if (err) {
 						throw err;
 					}
@@ -166,28 +189,3 @@ describe('SDK', function() {
 		})
 	})
 })
-var create_payment_json_paypal = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "transactions": [
-        {
-            "amount": {
-                "total": "7",
-                "currency": "USD",
-                "details": {
-                    "subtotal": "5",
-                    "tax": "1",
-                    "shipping": "1"
-                }
-            },
-            "description": "This is the payment transaction description."
-        }
-    ],
-    "redirect_urls": {
-        "return_url": "http://localhost/paymentwithpaypal?guid=b64644f236ed484fb047ec3439fb7a3f",
-        "cancel_url": "http://localhost/paymentwithpaypal?guid=b64644f236ed484fb047ec3439fb7a3f"
-    }
-};
-		
