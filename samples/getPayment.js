5da7eb9b20cc41637a5117f312976574d126c62b
Prasanna Annamalai <prannamalai.x.com@gmail.com>
2013-04-03 16:35:11 +0530

    removed vars, fixes for jshint

--- samples/getPayment.js
+++ samples/getPayment.js
@@ -1,14 +1,9 @@
 /* Copyright 2013 PayPal */
 "use strict";
 
-// Sandbox Creds - 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM', 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
-// VPS 16 Creds - 'EB6t3j2rIX5k59S6z2YMznpIBsel5qLWCwE88TT1HhKY', 'EGgy5gXmQC5Wtibks-81aJZIBsel5qLWCwE88TT1HhKY'
-
 var paypal_api = require('../');
 
-var token = null;
-
-var http_default_opts = {
+var config_opts = {
 	'host': 'api.sandbox.paypal.com',
 	'port': '',
 	'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
@@ -18,7 +13,7 @@ var http_default_opts = {
 var paymentId = "PAY-0XL713371A312273YKE2GCNI";
 
 
-paypal_api.payment.get(paymentId, http_default_opts, function(get_err, get_res) {
+paypal_api.payment.get(paymentId, config_opts, function (get_err, get_res) {
 	if (get_err) {
 		throw get_res;
 	}
