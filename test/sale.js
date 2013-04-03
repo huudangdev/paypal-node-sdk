9e930f0809893108bef531771ce57a3fc0bf4953
Prasanna Annamalai <prannamalai.x.com@gmail.com>
2013-04-03 16:25:30 +0530

    Fixed JSHint TC Fixes

--- test/sale.js
+++ test/sale.js
@@ -1,3 +1,6 @@
+/* Copyright 2013 PayPal */
+"use strict";
+
 var chai = require('chai'),
 	expect = chai.expect,
 	should = chai.should();
@@ -56,12 +59,12 @@ var create_payment_data = {
 };
 
 
-describe('SDK', function() {
-	describe('#Sale api ', function() {
-		it('Get and refund', function(done) {
+describe('SDK', function () {
+	describe('#Sale api ', function () {
+		it('Get and refund', function (done) {
 
 			// Making a payment 
-			paypal_sdk.payment.create(create_payment_data, config_opts, function(err, res) {
+			paypal_sdk.payment.create(create_payment_data, config_opts, function (err, res) {
 				if (err) {
 					console.log(err);
 				}
@@ -75,7 +78,7 @@ describe('SDK', function() {
 					console.log("sale_id : " + sale_id);
 
 					// Getting a sale transaction for a completed payment
-					paypal_sdk.sale.get(sale_id, config_opts, function(err, res) {
+					paypal_sdk.sale.get(sale_id, config_opts, function (err, res) {
 						if (err) {
 							console.log(err);
 						}
@@ -85,7 +88,7 @@ describe('SDK', function() {
 							console.log(res);
 							should.exist(res.id);
 							// Refund a sale 
-							paypal_sdk.sale.refund(res.id, refund_data, config_opts, function(err, res) {
+							paypal_sdk.sale.refund(res.id, refund_data, config_opts, function (err, res) {
 								if (err) {
 									console.log(err);
 								}
@@ -97,15 +100,15 @@ describe('SDK', function() {
 									done();
 								}
 
-							})
+							});
 						}
 
-					})
+					});
 				}
 
-			})
+			});
 
 
-		})
-	})
-})
+		});
+	});
+});
