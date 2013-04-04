78991c9dd6cc4e0ed01abb14dbf3fe0d20f627fb
siddick <mebramsha@paypal.com>
2013-04-04 15:47:52 +0530

    Now http_options is optional

--- test/generate_token.js
+++ test/generate_token.js
@@ -10,18 +10,17 @@ var paypal_sdk = require('../');
 
 var config_opts = {
 	'host': 'api.sandbox.paypal.com',
-	'port': ''
+	'port': '',
+  'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
+  'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
 };
-
-var client_id = 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM';
-var client_secret = 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM';
 
 
 
 describe('SDK', function () {
 	describe('#generateToken', function () {
 		it('token should be generated', function (done) {
-			paypal_sdk.generateToken(client_id, client_secret, config_opts, function (generatedToken) {
+			paypal_sdk.generateToken(config_opts, function (generatedToken) {
 
 				should.exist(generatedToken);
 				expect(generatedToken).to.contain('Bearer');
