150311073fb1fb2d9cc16582e0e29acd629beda4
siddick <mebramsha@paypal.com>
2013-05-28 10:04:01 +0530

    Added samples for authorization and capture

---
+++ samples/authorization/void.js
@@ -0,0 +1,10 @@
+var paypal_sdk = require('../../');
+require('../configure');
+
+paypal_sdk.authorization.void("6CR34526N64144512", function(error, authorization){
+  if(error){
+    console.error(error);
+  } else {
+    console.log(authorization);
+  }
+});
---
+++ samples/capture/refund.js
@@ -0,0 +1,16 @@
+var paypal_sdk = require('../../');
+require('../configure');
+
+var refund_details = {
+  "amount": {
+    "currency": "USD",
+    "total": "110.54" }
+};
+
+paypal_sdk.capture.refund("8F148933LY9388354", refund_details, function(error, refund){
+  if(error){
+    console.error(error);
+  } else {
+    console.log(refund);
+  }
+});
