2ef9dad6632fe5b05a208e679b5f476a2fb9f56b
siddick <mebramsha@paypal.com>
2013-05-28 09:24:56 +0530

    Removed duplicate create credit-card code

--- test/vault.js
+++ test/vault.js
@@ -8,41 +8,44 @@ var chai = require('chai'),
 var paypal_sdk = require('../');
 require('./configure');
 
-var credit_card_details = {
-  "type": "visa",
-  "number": "4417119669820331",
-  "expire_month": "11",
-  "expire_year": "2018",
-  "cvv2": "123" };
 
 describe('SDK', function () {
   describe('CreditCard', function () {
-    it('create and get', function (done) {
 
+    var credit_card_details = {
+      "type": "visa",
+      "number": "4417119669820331",
+      "expire_month": "11",
+      "expire_year": "2018",
+      "cvv2": "123" };
+
+    function create_credit_card(callback){
       paypal_sdk.credit_card.create(credit_card_details, function (error, credit_card) {
         expect(error).equal(null);
         expect(credit_card.id).to.contain('CARD');
+        callback(credit_card);
+      });
+    }
 
+    it('create and get', function (done) {
+
+      create_credit_card(function (credit_card) {
         paypal_sdk.credit_card.get(credit_card.id, function (error, credit_card) {
           expect(error).equal(null);
           expect(credit_card.id).to.contain('CARD');
           done();
         });
-
-      })
+      });
 
     });
+
     it('create and delete', function(done) {
 
-      paypal_sdk.credit_card.create(credit_card_details, function (error, credit_card) {
-        expect(error).equal(null);
-        expect(credit_card.id).to.contain('CARD');
-
+      create_credit_card(function (credit_card) {
         paypal_sdk.credit_card.delete(credit_card.id, function(error, no_response) {
           expect(error).equal(null);
           done();
         });
-
       });
 
     });
