7896c2ac9c640c9a6334bfe6235e2f0e54105dac
Avi Das <adas@colgate.edu>
2014-03-31 13:33:45 -0500

    Add complete future payment example

--- samples/payment/create_future_payment.js
+++ samples/payment/create_future_payment.js
@@ -1,30 +1,46 @@
-/* Copyright 2013 PayPal */
+/* Copyright 2014 PayPal */
 "use strict";
 var paypal_api = require('../../');
 require('../configure');
 
-var create_payment_json = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "transactions": [{
-        "amount": {
-            "currency": "USD",
-            "total": "1.00"
-        },
-        "description": "This is the payment description."
-    }]
-};
+var auth_code = {'authorization_code': 'Authorization code from the mobile sdk'};
 
-refresh_token = ''
-correlation_id = ''
-
-paypal_api.payment.create(create_payment_json, refresh_token, correlation_id, function (error, payment) {
+paypal_api.generate_token(auth_code, function (error, rt) {
     if (error) {
-        throw error;
-    } else {
-        console.log("Create Payment Response");
-        console.log(payment);
+        console.log(error);
+        console.log(error.response);
+    }  else {
+        //Refresh token has long shelf life. It is recommended to store this in a database
+        //for future payments
+        console.log(rt);
+        var refresh_token = rt;
+
+        var future_payment_config = {'correlation_id': 'Correlation ID from mobile sdk', 'refresh_token': refresh_token};
+
+        //Set up payment details
+        var create_payment_json = {
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "transactions": [{
+                "amount": {
+                    "currency": "USD",
+                    "total": "1.00"
+                },
+                "description": "This is the payment description."
+            }]
+        };
+
+        //Create future payment
+        paypal_api.payment.create(create_payment_json, future_payment_config, function (error, payment) {
+            if (error) {
+                console.log(error.response);
+                throw error;
+            } else {
+                console.log("Create Payment Response");
+                console.log(payment);
+            }
+        });
     }
 });
