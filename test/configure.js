a91887f322a5c603eda9b86c234c2fe6cad0807d
Avi Das <adas@colgate.edu>
2014-04-21 11:21:30 -0500

    update with mode

--- test/configure.js
+++ test/configure.js
@@ -3,8 +3,7 @@
 var paypal_sdk = require('../');
 
 paypal_sdk.configure({
-    'host': 'api.sandbox.paypal.com',
-    'port': '',
+	'mode': 'sandbox', //sandbox or live
     'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
     'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
 });
