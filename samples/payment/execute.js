b8755a32bdb9bc7549586156fd0bd0da1c292a97
Avi Das <adas@colgate.edu>
2014-05-05 17:51:20 -0500

    Bump version

---
+++ samples/payment/execute.js
@@ -0,0 +1,59 @@
+/* Copyright 2013 PayPal */
+"use strict";
+
+var paypal_api = require('../../');
+require('../configure');
+
+var create_payment_json = {
+    "intent": "authorize",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://return.url",
+        "cancel_url": "http://cancel.url"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "item",
+                "sku": "item",
+                "price": "1.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "1.00"
+        },
+        "description": "This is the payment description."
+    }]
+};
+
+paypal_api.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+    	console.log(error.response);
+        throw error;
+    } else {
+    	for (var index=0; index < payment.links.length; index++) {
+    		//Redirect user to this endpoint for redirect url
+    		if (payment.links[index].rel == 'approval_url')
+    			console.log(payment.links[index].href);
+    	}
+        console.log(payment);
+    }
+});
+
+
+var paymentId = 'PAYMENT id created in previous step';
+
+paypal_api.payment.execute(paymentId, { "payer_id" : "Appended to redirect url" }, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+        throw error;
+    } else {
+        console.log("Get Payment Response");
+        console.log(JSON.stringify(payment));
+    }
+});
