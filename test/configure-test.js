1e8139d585d5675b408c9efd97ed7d5cdf831b2f
Avi Das <adas@colgate.edu>
2014-07-15 18:04:18 -0500

    linting

--- test/configure-test.js
+++ test/configure-test.js
@@ -8,7 +8,7 @@ var paypal_sdk = require('../');
 require('./configure');
 
 describe('Configure Test', function () {
-    it ('Should return default options', function () {
+    it('Should return default options', function () {
         var config = paypal_sdk.configuration;
         config.should.be.a('object');
     });
