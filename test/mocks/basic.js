da8be4d667f673f78a0169348931a597e6fac48d
Bryant Luk <bryant.luk@getbraintree.com>
2016-01-11 15:19:06 -0600

    Remove invalid test mock

--- test/mocks/basic.js
+++
@@ -1,9 +0,0 @@
-var nock = require('nock');
-
-nock('https://tlscheck.chargify.com:443')
-  .get('/')
-  .reply(200, "ConnectionOK\n", { 'content-type': 'text/plain; charset=utf-8',
-  date: 'Wed, 21 Oct 2015 20:00:12 GMT',
-  'x-content-type-options': 'nosniff',
-  'content-length': '13',
-  connection: 'Close' });
